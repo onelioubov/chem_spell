c14ec2dfc4fadb5450b27cea3db36fff651953cc
Olga <onelioubov@gmail.com>
2017-02-10 20:27:51 -0500

    added tab delimited list of elements and element dictionary

--- chem_spell.py
+++ chem_spell.py
@@ -1,6 +1,19 @@
 word = input('Enter your word: ')
+element_dictionary = {}
+
+def get_element():
+    element_list = open('elements', 'r')
+    for line in element_list:
+        words = line.split('\t')
+        element_name = words[0]
+        element_symbol = words[1]
+        element_dictionary[element_name] = element_symbol
 
 def get_word(word):
 	print(word)
 
+def match_element():
+    return;
+get_element()
 get_word(word)
+print(element_dictionary)
