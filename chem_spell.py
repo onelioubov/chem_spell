9e52bf9dfa2ed232de4729ff90b46cef318cdc0e
Olga <onelioubov@gmail.com>
2017-02-10 23:36:29 -0500

    implemented preliminary matching

--- chem_spell.py
+++ chem_spell.py
@@ -1,19 +1,34 @@
 word = input('Enter your word: ')
 element_dictionary = {}
-
 def get_element():
     element_list = open('elements', 'r')
     for line in element_list:
         words = line.split('\t')
-        element_name = words[0]
-        element_symbol = words[1]
-        element_dictionary[element_name] = element_symbol
+        element_dictionary[words[1]] = words[0]
+    return element_dictionary
+
+def match_element(iter_word,new_string):
+    iter_word = list(iter_word)
+    while iter_word:
+        letter = iter_word[0].upper()
+        if letter in element_dictionary:
+            new_string += letter
+            iter_word.pop(0)
+        elif len(iter_word) > 1 and letter + iter_word[1] in element_dictionary:
+            new_string += letter
+            new_string += iter_word[1]
+            iter_word.pop(0)
+            iter_word.pop(0)
+        else:
+            iter_word.pop()
+            new_string += "?"
+    print (new_string)
 
 def get_word(word):
-	print(word)
+    new_string = ""
+    match_element(word,new_string)
+    return new_string
 
-def match_element():
-    return;
 get_element()
 get_word(word)
-print(element_dictionary)
+#print(final)
